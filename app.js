0bbbb5fa9d13beb0f05243cd5df6a5ce2a96f474
Kevin <ivexnuke@gmail.com>
2014-09-22 05:17:41 -0400

    Created and half completed HTML, CSS and JS files

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * @author Kevin
+ */
+var main = function() {
+	$('div.tab').click(function (){
+		var clickedTab = $('#'+this.getAttribute('id'));
+		var currentTab = $('#'+$('div.selected')[0].getAttribute('id'));
+		
+		currentTab.toggleClass('selected');
+		currentTab.toggleClass('unselected')
+		clickedTab.toggleClass('selected');
+		clickedTab.toggleClass('unselected')
+		
+		for(i=0; i<4; i++){
+			if(clickedTab.hasClass('unselected-' + (i + 1))){
+				currentTab.toggleClass('unselected-0');
+				clickedTab.toggleClass('unselected-0');
+				currentTab.toggleClass('unselected-' + (i + 1));
+				clickedTab.toggleClass('unselected-' + (i + 1));
+				break;
+			}
+		}
+	});	
+}
+$(document).ready(main);
