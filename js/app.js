d7ef9b600781289f4213d8fe211a4e13ce9d3775
Yi Fei Xue <yf3xue@uwaterloo.ca>
2017-08-08 12:10:08 -0400

    removed need for shown class, fixed visibility:collapse inconsistency using display:none for hidden

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 var hoverText = $("#hover-text");
 function showHoverText(ev) {
-	hoverText.removeClass("hidden").addClass("shown");
+	hoverText.removeClass("hidden");
 
 };
 function hideHoverText(ev){
-	hoverText.removeClass("shown").addClass("hidden");
+	hoverText.addClass("hidden");
 }
 function filterPressed(ev){
 	var filter = ev.delegateTarget.id;
 	if(filter != "home"){
-		$(".wrapper .card, .wrapper .card-stack").not(".always-on, .always-on .card").not("." + filter + ", ." + filter + " .card").removeClass("shown").addClass("hidden");
-		$(".wrapper .card." + filter + ", .wrapper .card-stack." + filter + ", .wrapper .card-stack." + filter + " .card").removeClass("hidden").addClass("shown");
+		$(".wrapper .card, .wrapper .card-stack").not(".always-on, .always-on .card").not("." + filter + ", ." + filter + " .card").addClass("hidden");
+		$(".wrapper .card." + filter + ", .wrapper .card-stack." + filter + ", .wrapper .card-stack." + filter + " .card").removeClass("hidden");
 	}
 	else{
-		$(".wrapper .card, .wrapper .card-stack, .wrapper .card-stack .card").removeClass("hidden").addClass("shown");
+		$(".wrapper .card, .wrapper .card-stack, .wrapper .card-stack .card").removeClass("hidden")
 	}
 }
 function cardStackPressed(ev){
